a4df5f26599cb6590ad0ba291d730edd7aea191b
arkavo <arkavohait@gmail.com>
2023-11-29 16:29:27 +0530

    fix

--- src/test_main.py
+++ src/test_main.py
@@ -4,8 +4,8 @@ import numpy as np
 import tqdm as tqdm
 
 test_mc0 = cst.MonteCarlo(config="../configs/test_config.json")
-
 test_mc0.mc_init()
+test_mc0.display_material()
 for i in tqdm.tqdm(range(test_mc0.S_Wrap), desc="Stability Runs", unit="runs", colour="green"):
     test_mc0.generate_random_numbers(test_mc0.stability_runs)
-    np.save(f"{test_mc0.save_direcotry}/grid_{i:04d}", test_mc0.run_mc_dmi_36362(test_mc0.T[0]))
+    np.save(f"{test_mc0.save_direcotry}/grid_{i:04d}", test_mc0.run_mc_dmi_66612(test_mc0.T[0]))
